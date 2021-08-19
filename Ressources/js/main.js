e8b6f0a2dc9c76d4f64acc48f86d5e5d501e493c
MrTimux62 <73614994+MrTimux62@users.noreply.github.com>
2021-08-19 14:18:31 +0200

    Add files via upload

--- Ressources/js/main.js
+++ Ressources/js/main.js
@@ -16,17 +16,28 @@ let timerMinutes = 30;
 let audioplay = 0;
 let audioplay2 = 0;
 let ennemyUnitsBase = 10
+let MineBoost = 0;
+let FermeBoost = 0;
+let BucheronBoost = 0;
 
+/**
+ * Fonction génération nombres aléatoire
+ * @param {*} max 
+ * @returns 
+ */
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
 $(document).ready(function () {
-    $('#Bienvenue').modal({backdrop: 'static', keyboard: false})
+    $('#Bienvenue').modal({ backdrop: 'static', keyboard: false })
     $("#Bienvenue").modal("show");
 });
 
-$("#startbutton").click(function (e) { 
+/**
+ * Lancement des compteurs et générations de ressources
+ */
+$("#startbutton").click(function (e) {
     updateRessources();
     startTimer();
     Ressources();
@@ -37,6 +48,9 @@ $("#startbutton").click(function (e) {
     }, 5000);
 });
 
+/**
+ * Si aucune valeur dans le stockage initialisé les valeurs
+ */
 if (localStorage.getItem("Bois") == null) {
     localStorage.setItem("Bois", 50);
     localStorage.setItem("Or", 50);
@@ -47,6 +61,9 @@ if (localStorage.getItem("Bois") == null) {
 
 // PARTIE UPDATE
 
+/**
+ * Si les ressources sont stocké les transposé dans le jeu
+ */
 function updateRessources() {
     if (localStorage.getItem("Bois") != null) {
         Bois = localStorage.getItem("Bois");
@@ -63,6 +80,9 @@ function updateRessources() {
     updateNav();
 }
 
+/**
+ * Sauvegarde du progrès des constructions
+ */
 function saveBuildProgress() {
     if (localStorage.getItem("caserneProgress") == null || localStorage.getItem("caserneProgress") == 0) {
         localStorage.setItem("caserneProgress", 0);
@@ -110,6 +130,9 @@ function saveBuildProgress() {
     }, 1000);
 }
 
+/**
+ * Permet sauvegardé les ressouces du jeu dans le stockage
+ */
 function setRessources() {
     if (Bois > 300) {
         Bois = 300
@@ -138,12 +161,51 @@ function setRessources() {
     updateNav();
 }
 
+/**
+ * Met à jour la navbar avec les ressources actuel
+ */
 function updateNav() {
     $("#WoodNav").val(parseInt(Bois) + "/300");
     $("#GoldNav").val(parseInt(Or) + "/300");
     $("#FoodNav").val(parseInt(Nourriture) + "/300");
     $("#UnitsNav").text("Unités " + parseInt(Unites) + "/200");
     //$("#TimeNav").val(Bois);
+}
+
+/**
+ * Update des modals
+ */
+ function modalUpdate() {
+    if ($("#NeedWood").val() == 0) {
+        $("#NeedWood").css("color", "white");
+    } else {
+        $("#NeedWood").val() > Bois ? $("#NeedWood").css("color", "red") : $("#NeedWood").css("color", "green");
+    }
+    if ($("#NeedGold").val() == 0) {
+        $("#NeedGold").css("color", "white");
+    } else {
+        $("#NeedGold").val() > Or ? $("#NeedGold").css("color", "red") : $("#NeedGold").css("color", "green");
+    }
+    if ($("#NeedFood").val() == 0) {
+        $("#NeedFood").css("color", "white");
+    } else {
+        $("#NeedFood").val() > Nourriture ? $("#NeedFood").css("color", "red") : $("#NeedFood").css("color", "green");
+    }
+    if ($("#BoostNeedWood").val() == 0) {
+        $("#BoostNeedWood").css("color", "white");
+    } else {
+        $("#BoostNeedWood").val() > Bois ? $("#BoostNeedWood").css("color", "red") : $("#BoostNeedWood").css("color", "green");
+    }
+    if ($("#BoostNeedGold").val() == 0) {
+        $("#BoostNeedGold").css("color", "white");
+    } else {
+        $("#BoostNeedGold").val() > Or ? $("#BoostNeedGold").css("color", "red") : $("#BoostNeedGold").css("color", "green");
+    }
+    if ($("#BoostNeedFood").val() == 0) {
+        $("#BoostNeedFood").css("color", "white");
+    } else {
+        $("#BoostNeedFood").val() > Nourriture ? $("#BoostNeedFood").css("color", "red") : $("#BoostNeedFood").css("color", "green");
+    }
 }
 
 // PARTIE MODAL
@@ -227,24 +289,6 @@ $('#Ferme').click(function (e) {
     modalUpdate();
 });
 
-function modalUpdate() {
-    if ($("#NeedWood").val() == 0) {
-        $("#NeedWood").css("color", "white");
-    } else {
-        $("#NeedWood").val() > Bois ? $("#NeedWood").css("color", "red") : $("#NeedWood").css("color", "green");
-    }
-    if ($("#NeedGold").val() == 0) {
-        $("#NeedGold").css("color", "white");
-    } else {
-        $("#NeedGold").val() > Or ? $("#NeedGold").css("color", "red") : $("#NeedGold").css("color", "green");
-    }
-    if ($("#NeedFood").val() == 0) {
-        $("#NeedFood").css("color", "white");
-    } else {
-        $("#NeedFood").val() > Nourriture ? $("#NeedFood").css("color", "red") : $("#NeedFood").css("color", "green");
-    }
-}
-
 /**
  * Acheter un batiment
  * @param {*} batiment 
@@ -286,6 +330,42 @@ function buyBuilding(batiment) {
         console.log($("#NeedGold").val());
         console.log($("#GoldNav").val());
         console.log($("#NeedFood").val());
+        console.log($("#FoodNav").val());
+    }
+}
+
+/**
+ * Boost un batiment de production
+ * @param {*} batiment 
+ */
+function BoostBuilding(batiment) {
+    if (parseInt($("#BoostNeedWood").val()) <= parseInt($("#WoodNav").val()) && parseInt($("#BoostNeedGold").val()) <= parseInt($("#GoldNav").val()) && parseInt($("#BoostNeedFood").val()) <= parseInt($("#FoodNav").val())) {
+        Bois = parseInt(Bois) - $("#BoostNeedWood").val();
+        Or = parseInt(Or) - $("#BoostNeedGold").val();
+        Nourriture = parseInt(Nourriture) - $("#BoostNeedFood").val();
+        setRessources();
+        console.log("Batiment : " + batiment);
+        switch (batiment) {
+            case "Bucheron":
+                boostBucheron();
+                break;
+            case "Ferme":
+                boostFerme();
+                break;
+            case "Mine":
+                boostMine();
+                break;
+            default:
+                break;
+        }
+        $("#BoostModal").modal("hide");
+    } else {
+        alert("Ressources Insuffisantes");
+        console.log($("#BoostNeedWood").val());
+        console.log($("#WoodNav").val());
+        console.log($("#BoostNeedGold").val());
+        console.log($("#GoldNav").val());
+        console.log($("#BoostNeedFood").val());
         console.log($("#FoodNav").val());
     }
 }
@@ -460,21 +540,30 @@ function Ressources() {
     setInterval(() => {
         if (Bois < 300) {
             (bucheronProgress >= bucheronTime) ? Bois = parseInt(Bois) + 2 : Bois = parseInt(Bois) + 1;
+            if (BucheronBoost == 1) {
+                Bois = Bois + 1;
+            }
             setRessources();
             updateNav();
         }
         if (Or < 300) {
             (mineProgress >= mineTime) ? Or = parseInt(Or) + 2 : Or = parseInt(Or) + 1;
+            if (MineBoost == 1) {
+                Or = Or + 1;
+            }
             setRessources();
             updateNav();
         }
         if (Nourriture < 300) {
             (fermeProgress >= fermeTime) ? Nourriture = parseInt(Nourriture) + 2 : Nourriture = parseInt(Nourriture) + 1;
+            if (FermeBoost == 1) {
+                Nourriture = Nourriture + 1;
+            }
             setRessources();
             updateNav();
         }
         modalUpdate();
-    }, 6000);
+    }, 8000);
 }
 function unitsRessources() {
     setInterval(() => {
@@ -484,7 +573,7 @@ function unitsRessources() {
             setRessources();
             updateNav();
         }
-    }, 10000);
+    }, 12000);
 }
 
 $(".Base1").click(function (e) {
@@ -492,6 +581,8 @@ $(".Base1").click(function (e) {
     var audio = new Audio('Pack Sons/base.mp3');
     audio.play();
     $("#SelectImg").attr("src", "Pack Images/Chateau Bleu.png");
+    $("#btn1").css("display", "")
+    $("#btn2").css("display", "")
     $("#btn1").attr("onclick", "lancerAttack()")
     $("#btn1text").text("Lancer une attaque")
     $("#btn2").attr("onclick", "retourMenu()")
@@ -503,52 +594,265 @@ $("#caserneBuild").click(function (e) {
     var audio = new Audio('Pack Sons/caserne.mp3');
     audio.play();
     $("#SelectImg").attr("src", "Pack Images/Barracks.png");
-    $("#btn1").attr("onclick", "recrutUnit()")
-    $("#btn1text").text("Recruter des unités")
-    $("#btn2").attr("onclick", "repairCaserne()")
-    $("#btn2text").text("Réparer")
+    if (caserneProgress >= caserneTime) {
+        $("#btn1").css("display", "")
+        $("#btn2").css("display", "")
+        $("#btn1").attr("onclick", "recrutUnit()")
+        $("#btn1text").text("Recruter des unités")
+        $("#btn2").attr("onclick", "repairCaserne()")
+        $("#btn2text").text("Réparer")
+    } else {
+        $("#btn1").css("display", "none")
+        $("#btn2").css("display", "none")
+    }
+
 });
 $("#archerieBuild").click(function (e) {
     e.preventDefault();
     var audio = new Audio('Pack Sons/archerie.mp3');
     audio.play();
     $("#SelectImg").attr("src", "Pack Images/Archery.png");
+    if (archerieProgress >= archerieTime) {
+        $("#btn1").css("display", "")
+        $("#btn2").css("display", "")
+        $("#btn1").attr("onclick", "recrutUnit()")
+        $("#btn1text").text("Recruter des unités")
+        $("#btn2").attr("onclick", "repairArcherie()")
+        $("#btn2text").text("Réparer")
+    } else {
+        $("#btn1").css("display", "none")
+        $("#btn2").css("display", "none")
+    }
 });
 $("#mineBuild").click(function (e) {
     e.preventDefault();
     var audio = new Audio('Pack Sons/mine.mp3');
     audio.play();
     $("#SelectImg").attr("src", "Pack Images/Gold_mine.png");
+    if (mineProgress >= mineTime) {
+        $("#btn1").css("display", "");
+        $("#btn2").css("display", "");
+        $("#BoostNeedGold").val(15);
+        $("#BoostNeedFood").val(15);
+        $("#BoostNeedWood").val(15);
+        $("#BoostTitle").text("Production Mine d'or");
+        $("#BoostDescription").text("Permet d'obtenir un boost de production d'or pendant le temps indiqué")
+        $("#BoostButton").attr("name", "Mine")
+        $("#BoostButton").attr("onclick", "BoostBuilding(this.name)")
+        $("#btn1").attr("onclick", "boostModal()");
+        $("#btn1text").text("Booster la production");
+        $("#btn2").attr("onclick", "buyRessources()");
+        $("#btn2text").text("Acheter");
+    } else {
+        $("#btn1").css("display", "none");
+        $("#btn2").css("display", "none");
+    }
 });
 $("#ecurieBuild").click(function (e) {
     e.preventDefault();
     var audio = new Audio('Pack Sons/ecurie.mp3');
     audio.play();
     $("#SelectImg").attr("src", "Pack Images/Stable.png");
+    if (ecurieProgress >= ecurieTime) {
+        $("#btn1").css("display", "")
+        $("#btn2").css("display", "")
+        $("#btn1").attr("onclick", "recrutUnit()")
+        $("#btn1text").text("Recruter des unités")
+        $("#btn2").attr("onclick", "repairEcurie()")
+        $("#btn2text").text("Réparer")
+    } else {
+        $("#btn1").css("display", "none")
+        $("#btn2").css("display", "none")
+    }
 });
 $("#fermeBuild").click(function (e) {
     e.preventDefault();
     var audio = new Audio('Pack Sons/ferme.mp3');
     audio.play();
     $("#SelectImg").attr("src", "Pack Images/Mill.png");
+    if (fermeProgress >= fermeTime) {
+        $("#btn1").css("display", "")
+        $("#btn2").css("display", "")
+        $("#BoostNeedGold").val(15);
+        $("#BoostNeedFood").val(15);
+        $("#BoostNeedWood").val(15);
+        $("#BoostTitle").text("Production Ferme");
+        $("#BoostDescription").text("Permet d'obtenir un boost de production de nourritures pendant le temps indiqué")
+        $("#BoostButton").attr("name", "Ferme")
+        $("#BoostButton").attr("onclick", "BoostBuilding(this.name)")
+        $("#btn1").attr("onclick", "boostModal()");
+        $("#btn1text").text("Booster la production");
+        $("#btn2").attr("onclick", "sellFood()");
+        $("#btn2text").text("Vendre");
+    } else {
+        $("#btn1").css("display", "none")
+        $("#btn2").css("display", "none")
+    }
 });
 $("#bucheronBuild").click(function (e) {
     e.preventDefault();
     var audio = new Audio('Pack Sons/bucheron.mp3');
     audio.play();
     $("#SelectImg").attr("src", "Pack Images/Lumberjack.png");
+    if (bucheronProgress >= bucheronTime) {
+        $("#btn1").css("display", "")
+        $("#btn2").css("display", "")
+        $("#BoostNeedGold").val(15);
+        $("#BoostNeedFood").val(15);
+        $("#BoostNeedWood").val(15);
+        $("#BoostTitle").text("Production Bucheron");
+        $("#BoostDescription").text("Permet d'obtenir un boost de production de bois pendant le temps indiqué")
+        $("#BoostButton").attr("name", "Bucheron")
+        $("#BoostButton").attr("onclick", "BoostBuilding(this.name)")
+        $("#btn1").attr("onclick", "boostModal()");
+        $("#btn1text").text("Booster la production");
+        $("#btn2").attr("onclick", "sellWood()");
+        $("#btn2text").text("Vendre");
+    } else {
+        $("#btn1").css("display", "none")
+        $("#btn2").css("display", "none")
+    }
 });
+
+function buyRessources() {
+    $("#BuyRessourcesModal").modal("show");
+}
+
+function sellWood() {
+    $("#SellWoodModal").modal("show");
+}
+
+function sellFood() {
+    $("#SellFoodModal").modal("show");
+}
+
+function nbSellWood(nb) {
+    $("#nbGoldWood").text(nb + " Or");
+    $("#NeedWoodSell").val(nb);
+    if (Bois >= nb) {
+        $("#NeedWoodSell").css("color", "green");
+    } else {
+        $("#NeedWoodSell").css("color", "red");
+    }
+}
+
+function nbSellFood(nb) {
+    $("#nbGoldFood").text(nb + " Or");
+    $("#NeedFoodSell").val(nb);
+    if (Nourriture >= nb) {
+        $("#NeedFoodSell").css("color", "green");
+    } else {
+        $("#NeedFoodSell").css("color", "red");
+    }
+}
+
+$("#SellWood").click(function (e) { 
+    e.preventDefault();
+    if (Bois >= $("#NeedWoodSell").val()) {
+        Bois = Bois - $("#NeedWoodSell").val();
+        Or = Or + parseInt($("#nbGoldWood").text());
+        setRessources();
+        updateNav();
+        $("#SellWoodModal").modal("hide");
+    }
+});
+
+$("#SellFood").click(function (e) { 
+    e.preventDefault();
+    if (Nourriture >= $("#NeedFoodSell").val()) {
+        Nourriture = Nourriture - $("#NeedFoodSell").val();
+        Or = Or + parseInt($("#nbGoldFood").text());
+        setRessources();
+        updateNav();
+        $("#SellFoodModal").modal("hide");
+    }
+});
+
+$("#BuyRessources").click(function (e) { 
+    e.preventDefault();
+    if (Or >=  $("#BuyGoldNeed").val() ){
+        Or = Or - $("#BuyGoldNeed").val();
+        Bois = Bois + parseInt(WoodBuy);
+        Nourriture = Nourriture + parseInt(FoodBuy);
+        setRessources()
+        updateNav()
+        $("#BuyRessourcesModal").modal("hide");
+    }
+});
+
+let WoodBuy = 0;
+let FoodBuy = 0;
+
+function nbWoodBuy(nb) {
+    WoodBuy = nb;
+    $("#nbWoodBuy").text(nb + " Bois");
+    $("#BuyGoldNeed").val(parseInt(FoodBuy) + parseInt(WoodBuy));
+    if (Or >= $("#BuyGoldNeed").val()) {
+        $("#BuyGoldNeed").css("color", "green");
+    } else {
+        $("#BuyGoldNeed").css("color", "red");
+    }
+}
+
+function nbFoodBuy(nb) {
+    FoodBuy = nb;
+    $("#nbFoodBuy").text(nb + " Nourritures");
+    $("#BuyGoldNeed").val(parseInt(FoodBuy) + parseInt(WoodBuy));
+    if (Or >= $("#BuyGoldNeed").val()) {
+        $("#BuyGoldNeed").css("color", "green");
+    } else {
+        $("#BuyGoldNeed").css("color", "red");
+    }
+}
+
+$("#SellWood").click(function (e) { 
+    e.preventDefault();
+    
+});
+
+function boostMine() {
+    if (MineBoost == 0 ) {
+        MineBoost = 1;
+        setTimeout(() => {
+            MineBoost = 0;
+        }, 60000);
+        $("#BoostModal").modal("hide");
+    }
+}
+function boostFerme() {
+    if (FermeBoost == 0) {
+        FermeBoost = 1;
+        setTimeout(() => {
+            FermeBoost = 0;
+        }, 60000);
+        $("#BoostModal").modal("hide");
+    }
+}
+function boostBucheron() {
+    if (BucheronBoost == 0) {
+        BucheronBoost = 1;
+        setTimeout(() => {
+            BucheronBoost = 0;
+        }, 60000);
+        $("#BoostModal").modal("hide");
+    }
+}
+
+function boostModal() {
+    modalUpdate();
+    $("#BoostModal").modal("show");
+}
 
 function retourMenu() {
     $("#AbandonModal").modal("show");
 }
 
-$("#winbutton").click(function (e) { 
+$("#winbutton").click(function (e) {
     e.preventDefault();
     cleardata();
     redirectIndex();
 });
-$("#losebutton").click(function (e) { 
+$("#losebutton").click(function (e) {
     e.preventDefault();
     cleardata();
     redirectIndex();
@@ -557,9 +861,6 @@ $("#losebutton").click(function (e) {
 function redirectIndex() {
     window.location.href = "index.html";
 }
-
-
-// PARTIE MUSIQUE AMBIANCE
 
 $("#htmlmain").click(function () {
     if (audioplay == 0) {
@@ -605,7 +906,7 @@ function nbSoldatsRecrut(nb) {
     $("#NeedFoodRecrut").val(nb);
 }
 
-$("#RecrutUnitbtn").click(function (e) { 
+$("#RecrutUnitbtn").click(function (e) {
     e.preventDefault();
     if (parseInt($("#nbSoldatsRecrut").text()) + Unites <= 200 && $("#NeedGoldRecrut").val() <= Or && $("#NeedFoodRecrut").val() <= Nourriture && caserneProgress >= caserneTime) {
         Unites = Unites + parseInt($("#nbSoldatsRecrut").text())
@@ -615,7 +916,7 @@ $("#RecrutUnitbtn").click(function (e) {
         setRessources();
         updateNav();
     }
-    
+
 });
 
 $("#LancementAttaque").click(function () {
